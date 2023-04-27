628d81aac78bab3d9d9c9376a006640beac6887a
Moras123 <diego.moron@ucb.edu.bo>
2023-04-27 11:19:06 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import {createApp} from 'vue'
 import {createRouter, createWebHistory} from 'vue-router'
 import App from './App.vue'
-import Jose from './components/Jose.vue'
-import Carlos from './components/Carlos.vue'
+import Jose from '../src/components/Jose.vue'
+import Carlos from '../src/components/Carlos.vue'
 
 const router = createRouter ({
     history: createWebHistory(),
